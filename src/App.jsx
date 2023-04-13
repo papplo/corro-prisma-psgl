d1dd625d0219fc5b8804585550a096c8bfae2a6f
Pablo Anttila <pablo@queenslab.se>
2023-04-14 01:16:48 +0200

    Fetch data and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import * as React from "react";
+import useSWR from 'swr'
 
 export default function App() {
   const [times, setTimes] = React.useState(0);
+
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+
+  const { data: products, error } = useSWR("/products", fetcher);
+
+  if (!products) {
+    return (<div>loading...</div>)};
+
   return (
-    <div>
+    <>
+    <header>
       <h1>Hello {times}</h1>
       <button onClick={() => setTimes((times) => times + 1)}>ADD</button>
-    </div>
+    </header>
+    <main>
+      {products && products.data.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.price} CLP</p>
+        </div>
+      ))}
+    </main>
+    </>
   );
 }
